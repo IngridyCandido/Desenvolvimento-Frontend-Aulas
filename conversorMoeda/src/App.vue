a430627af5d9c52703dcb91c26c10afa62e99767
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-12-02 16:31:33 -0300

    Add files via upload

---
+++ conversorMoeda/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { createApp, ref, computed } from 'vue';
+
+const Real = ref (0)
+
+const Dolar = computed(()=>{
+  return Real.value * 0.19
+})
+
+const Euro = computed (()=>{
+  return Real.value * 0.16
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>Informe o valor em real:</h1>
+    <div><input v-model="Real" type="number" placeholder="R$"></input></div>
+    <h2>O valor de R${{ Real }},00 em Dolar é {{ Dolar }}.</h2>
+    <h2>O valor de R${{ Real }},00 em Euro é {{ Euro }}.</h2>
+  </div>
+</template>
+
+<style scoped></style>
