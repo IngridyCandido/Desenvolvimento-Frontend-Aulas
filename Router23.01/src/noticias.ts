06ee69e12b211bdf6098682d4c338dffa526471e
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2026-02-05 17:47:31 -0300

    Add files via upload

---
+++ Router23.01/src/noticias.ts
@@ -0,0 +1,34 @@
+const noticias = [
+    {
+        id:1,
+        titulo: 'Notícia 1',
+        conteudo: 'Algum conteúdo qualquer'
+    },
+    {
+        id:2,
+        titulo: 'Notícia 2',
+        conteudo: 'Bom dia, não teremos aula hoje.'
+    },
+    {
+        id:3,
+        titulo: 'Notícia 3',
+        conteudo: 'Não conseguirei comparecer a nossa aula de 18/12/2025. Amanhã confirmo se consigo ou não participar da nossa aula da sexta-feira.'
+    },
+    {
+        id:4,
+        titulo: 'Notícia 4',
+        conteudo: 'Projeto no final da aula de 12/12/2025.'
+    },
+    {
+        id:5,
+        titulo: 'Notícia 5',
+        conteudo: 'Para quem se perdeu ou não pode participar da última aula, este foi o conteúdo de onde paramos. Vamos dar continuidade na aula de hoje.'
+    },
+    {
+        id:6,
+        titulo: 'Notícia 6',
+        conteudo: 'Mal humor hoje.'
+    },
+]
+
+export default noticias
---
+++ Router23.01/src/views/Noticias.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+   // import noticias from '@/noticias';
+    import { RouterLink} from 'vue-router';
+    import { onMounted, ref } from 'vue';
+
+    type Noticia = {
+      id:number,
+      titulo:string
+    }
+    const noticias = ref<Noticia[]> ([])
+
+    onMounted(async()=> {
+      const resultado = await fetch('https://api.spaceflightnewsapi.net/v4/articles/?format=json')
+      
+      const resposta = await resultado.json()
+
+      noticias.value = resposta.results.map((n:any)=> {
+        return{
+          id: n.id,
+          titulo: n.title
+        } as Noticia
+      })
+    })
+
+</script>
+
+<template>
+  <div class="about">
+    <h1>Notícias</h1>
+    <div>
+        <div v-for="noticia in noticias" :key="noticia.id">
+            <RouterLink :to="{ name: 'detalhe', params: { id: noticia.id } }">{{ noticia.titulo }}</RouterLink>
+        </div>    
+    </div>
+  </div>
+</template>
